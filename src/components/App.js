9d143a3e38c394778eccedb38d723f2d7fd8c9fb
timkinsman <tkinsm@gmail.com>
2020-08-03 14:40:00 +0930

    sections

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import About from './About';
+import Contact from './Contact';
+import Experience from './Experience';
+import Footer from './Footer';
+import Home from './Home';
+import Nav from './Nav';
+import Work from './Work';
+
 const App = () => {
   return (
     <div>
-      APP
+      <Nav />
+      <Home />
+      <About />
+      <Experience />
+      <Work />
+      <Contact />
+      <Footer />
     </div>
   );
 }
