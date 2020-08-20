03db9df83bf7c4a05a761d11c80e679838cee213
timkinsman <tkinsm@gmail.com>
2020-08-20 14:58:30 +0930

    flex styling nav and footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div style={{height: '5vh', textAlign: 'right'}}>© 2020 Timothy Kinsman</div>
+    <div style={{height: '5vh', display: 'flex', alignItems: 'center', flexDirection: 'row-reverse'}}>© 2020 Timothy Kinsman</div>
   );
 }
 
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div style={{display: 'block', height: '5vh'}}>
+    <div style={{display: 'flex', height: '5vh', alignItems: 'center'}}>
       <Link to="/">00. Timothy Kinsman</Link>
 
-      <div style={{float: 'right'}}>
+      <div style={{marginLeft: 'auto'}}>
         <Link to="/about">01. About</Link> <Link to="/experience">02. Experience</Link> <Link to="/work">03. Work</Link> <Link to="/contact">04. Contact</Link>
       </div>
     </div>
