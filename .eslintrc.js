498d90121150766db098088f1dac3ed2fd4f3d61
timkinsman <tkinsm@gmail.com>
2020-08-29 14:50:46 +0930

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,26 @@
 module.exports = {
-    "root": true,
     "env": {
         "browser": true,
-        "es2020": true,
-        "node": true
+        "es6": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
     "parserOptions": {
-        "ecmaVersion": 12
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
     },
+    "plugins": [
+        "react"
+    ],
     "rules": {
     }
 };
