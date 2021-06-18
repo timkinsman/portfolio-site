384eb2d5b273f7dc40c3fa512b1937008cc90691
timkinsman <tkinsm@gmail.com>
2021-06-18 18:28:23 +0930

    server and dev

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const buildPath = path.join(__dirname, "..", "build");
+app.use(express.static(buildPath));
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(buildPath, "index.html"));
+});
+
+app.listen(PORT, () => {
+   console.log("Server is up!", PORT);
+});
